707cde3f1c0fafc750dbf818eadbe30b7491fc4c
Rita Patrick <rita.patrick1007@gmail.com>
2018-03-01 22:45:31 -0600

    wip

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("hello")
+console.log("hello");
+
+// Challenge #3: Find the unique number
+// Given an array of more than 3 numbers where all numbers are equal except for one. Display the one unique number!
+
+var challengeThreeInputA = [ 1, 1, 1, 2, 1, 1 ];  // 2
+var challengeThreeInputB = [ 0, 0, 0, 0, 0, 0, 0, 0.55, 0, 0 ];  // 0.55
+
+
+if(challengeThreeInputA[q])===challengeThreeInputA[q+1])
