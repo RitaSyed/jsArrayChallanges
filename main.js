d8fbeff9d05dd2b5d15de7228f9f582377802e88
Rita Patrick <rita.patrick1007@gmail.com>
2018-03-02 15:48:43 -0600

    completed challenge 3

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ console.log("hello");
 
 var challengeThreeInputA = [ 1, 1, 1, 2, 1, 1 ];  // 2
 var challengeThreeInputB = [ 0, 0, 0, 0, 0, 0, 0, 0.55, 0, 0 ];  // 0.55
-
-
-if(challengeThreeInputA[q])===challengeThreeInputA[q+1])
+var emprtyArrayAnswer = [];
+var challenge3Answer = document.getElementById("challenge-3");
+for(var q=0; q<challengeThreeInputB.length; q++){
+  if(challengeThreeInputB[q] !== challengeThreeInputB[q-1] && challengeThreeInputB[q] !== challengeThreeInputB[q+1]){
+    emprtyArrayAnswer.push(challengeThreeInputB[q]);
+  }
+}
+challenge3Answer.innerHTML = "<h5>" + emprtyArrayAnswer + "<h5>";
