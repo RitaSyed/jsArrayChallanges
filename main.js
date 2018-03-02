e2b5705b5b09a17c9c4a481bd58e3506cc88e7d4
Rita Patrick <rita.patrick1007@gmail.com>
2018-03-02 10:31:58 -0600

    completed challenge 4

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("hello")
+// Challenge #4: Lost Without a Map
+// Given an array of any number of integers display the array with each value doubled.
+
+
+var challengeFourInputA = [ 1, 2, 3 ];  // [2, 4, 6]
+var challengeFourInputB = [ 3, 8, 1, 2, 4, 12 ];  // [ 6, 16, 2, 4, 8, 24 ]
+var challenge4AnswerA = [];
+var challenge4AnswerB = [];
+
+var answer = document.getElementById("challenge-4");
+for(var k=0; k<challengeFourInputA.length; k++){
+  challenge4AnswerA.push((challengeFourInputA[k]*2));
+}
+
+answer.innerHTML += "<h5>" + challenge4AnswerA + "</h5>";
+
+for(var k=0; k<challengeFourInputB.length; k++){
+  challenge4AnswerB.push((challengeFourInputB[k]*2));
+}
+
+answer.innerHTML += "<h5>" + challenge4AnswerB + "</h5>";
