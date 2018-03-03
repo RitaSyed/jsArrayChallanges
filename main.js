c7b046b3ce34a23dc16b19a5cc8f1d2fbcb34f49
Rita Patrick <rita.patrick1007@gmail.com>
2018-03-03 17:12:49 -0600

    final review

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("hello")
+// Challenge 6______________________________________________________
 // You're working in a number zoo, and it seems that one of the numbers has gone missing! Given an array of numbers. The numbers will be unsorted values between 1 and one more than the length of the array. No values will be repeated within the array. displaythe number that is missing.
 
 // Examples:
@@ -9,6 +9,7 @@ var test3 = [13,11,10,3,2,1,4,5,6,9,7,8]; // 12
 var answer = document.getElementById("challenge-6");
 var maxvalue = test3.length+1;
 var challenge6Answer;
+
 for(var i=1; i<maxvalue; i++){
   if(test3.indexOf(i) == -1){
     challenge6Answer= [i];
