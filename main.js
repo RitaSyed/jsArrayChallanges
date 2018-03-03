b3a1c25375ce18feb97b0515909dacdc1173554e
Rita Patrick <rita.patrick1007@gmail.com>
2018-03-03 16:21:18 -0600

    final review

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-// Challenge #3: Find the unique number
+// Challenge #3: Find the unique number_____________________________________________
 // Given an array of more than 3 numbers where all numbers are equal except for one. Display the one unique number!
 
 var challengeThreeInputA = [ 1, 1, 1, 2, 1, 1 ];  // 2
 var challengeThreeInputB = [ 0, 0, 0, 0, 0, 0, 0, 0.55, 0, 0 ];  // 0.55
 var emprtyArrayAnswer = [];
 var challenge3Answer = document.getElementById("challenge-3");
+
 for(var q=0; q<challengeThreeInputB.length; q++){
   if(challengeThreeInputB[q] !== challengeThreeInputB[q-1] && challengeThreeInputB[q] !== challengeThreeInputB[q+1]){
     emprtyArrayAnswer.push(challengeThreeInputB[q]);
